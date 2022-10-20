4bd846ef3e8ba168e1b397ad512df7f2cebe625d
asad <asadtahir9999@gmail.com>
2022-10-19 22:01:50 -0500

    Added commads for the application workflow.

--- monopoly_banking_system/Program.cs
+++ monopoly_banking_system/Program.cs
@@ -18,9 +18,30 @@ for (int i = 1; i <= numberOfPlayers; i++)
 {
     Console.WriteLine($"Please enter player {i}");
 
-    string playerName  = Console.ReadLine();
+    string playerName = Console.ReadLine();
 
     Player player = new Player(i, playerName, startingBalance);
 
     players.Add(player);
+}
+
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("Please enter a command");
+
+    Console.WriteLine("1. Print player details");
+
+    Console.WriteLine("2. Issue go pass credits");
+
+    Console.WriteLine("3. Collect rent for player");
+
+    Console.WriteLine("4. Sell property");
+
+    Console.WriteLine("5. Sell house");
+
+    string command = Console.ReadLine();
+    
+    Console.WriteLine(command);
 }
